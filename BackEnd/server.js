936dd0c132a78dd3c2078ab1b7a8649caa392d54
JaireshJV <jaireshjv16@gmail.com>
2025-04-13 21:11:06 +0530

    serveralter

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -51,7 +51,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
